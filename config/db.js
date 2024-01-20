0281df69fc3da8381711f512b6df13648e02724d
phuocdz5 <phuoccntt2004@gmail.com>
2024-01-21 01:14:04 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 
+// Sử dụng biến môi trường để đọc đường dẫn kết nối MongoDB
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/AssigmentReactNative';
 
-mongoose.connect('mongodb://localhost:27017/AssigmentReactNative', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // Bắt sự kiện khi kết nối thành công
 mongoose.connection.on('connected', () => {
-    console.log('Connected to MongoDB');
-  });
-  
-  // Bắt sự kiện khi có lỗi kết nối
-  mongoose.connection.on('error', (err) => {
-    console.error('Failed to connect to MongoDB', err);
-  });
+  console.log('Connected to MongoDB');
+});
 
+// Bắt sự kiện khi có lỗi kết nối
+mongoose.connection.on('error', (err) => {
+  console.error('Failed to connect to MongoDB', err);
+});
 
-  module.exports = mongoose.connection;
+module.exports = mongoose.connection;
