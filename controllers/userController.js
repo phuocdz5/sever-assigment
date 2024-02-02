ad3eb57071b42954eeab33d5a6fe4dc094d36692
phuocdz5 <phuoccntt2004@gmail.com>
2024-02-02 23:24:20 +0700

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ const UserController ={
             const {fullname,email, password,phone} = req.body ;
             const user = new UserModel({fullname,email, password,phone});
             await user.save();
-            res.status(200).json({message : 'success'+fullname});
+            res.status(200).json({message : 'success'});
         } catch (error) {
             res.status(500).json({error : error.message});
         }
